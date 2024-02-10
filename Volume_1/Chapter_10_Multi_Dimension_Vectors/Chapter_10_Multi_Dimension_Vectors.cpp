1773552a9ece5464d8e645aa540ab2e814510b8e
Marek Tokarz <marek.tokarz14@gmail.com>
2024-02-10 12:09:19 +0100

    Chapter_10_Multi_Dimensional_Vectors: NO DESCRIPTION - empty project so far.

---
+++ Volume_1/Chapter_10_Multi_Dimension_Vectors/Chapter_10_Multi_Dimension_Vectors.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Chapter_10_Multi_Dimensional_Vectors
+
+/* Description as in commit:
+
+Chapter_10_Multi_Dimensional_Vectors: NO DESCRIPTION - empty project so far
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+}
