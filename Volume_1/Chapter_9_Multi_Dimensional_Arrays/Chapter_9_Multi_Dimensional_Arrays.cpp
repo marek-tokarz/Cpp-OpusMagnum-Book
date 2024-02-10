6e72dbb70dc157b26758ee68f3db04c204e35b51
Marek Tokarz <marek.tokarz14@gmail.com>
2024-02-10 12:03:09 +0100

    Chapter_9_Multi_Dimensional_Arrays: NO DESCRIPTION - empty project so far.

---
+++ Volume_1/Chapter_9_Multi_Dimensional_Arrays/Chapter_9_Multi_Dimensional_Arrays.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Chapter_9_Multi_Dimensional_Arrays
+
+/* Description as in commit:
+
+Chapter_9_Multi_Dimensional_Arrays: NO DESCRIPTION - empty project so far
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+}
