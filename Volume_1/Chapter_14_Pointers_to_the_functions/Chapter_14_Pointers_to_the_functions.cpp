bd02a428a03804de185b5d08e9d3d1922e3ea8c3
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-02 13:38:20 +0200

    Chapter_14_Pointers_to_the_functions: NO DESCRIPTION - empty project so far

---
+++ Volume_1/Chapter_14_Pointers_to_the_functions/Chapter_14_Pointers_to_the_functions.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Chapter_14_Pointers_to_the_functions
+
+/* Description as in commit:
+Chapter_14_Pointers_to_the_functions: NO DESCRIPTION - empty project so far
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+}
