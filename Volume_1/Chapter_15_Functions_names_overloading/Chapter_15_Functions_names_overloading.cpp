9c81e553c2376e3e2d0f61a11654e9a2a47d91de
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-03 11:54:34 +0200

    Chapter_15_Functions_names_overloading: NO DESCRIPTION - empty project so far

---
+++ Volume_1/Chapter_15_Functions_names_overloading/Chapter_15_Functions_names_overloading.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Chapter_15_Functions_names_overloading
+
+/* Description as in commit:
+Chapter_15_Functions_names_overloading: NO DESCRIPTION - empty project so far
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+}
