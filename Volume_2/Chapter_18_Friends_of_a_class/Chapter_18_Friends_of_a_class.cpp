8cef5bf9f6af320dfe285fe76794ed2223be085c
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-04 13:32:06 +0200

    Initial commit of Chapter_18_Friends_of_a_class

---
+++ Volume_2/Chapter_18_Friends_of_a_class/Chapter_18_Friends_of_a_class.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Chapter_18_Friends_of_a_class
+
+/* Description as in commit:
+Chapter_18_Friends_of_a_class
+
+Add a description!
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+#define SEVENTH		0	//
+#define EIGHTH		0	//
+#define NINTH		0	//
+#define TENTH		0	//
+#define ELEVENTH	0	//
+#define TWELFTH		0	//
+#define THIRTEENTH	0	//
+#define	FOURTEENTH	0	//
+#define FIFTEENTH	0	//
+#define SIXTEENTH	0	//
+#define SEVENTEENTH	1	//
+
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTH) //
+	{
+	}
+
+	else
+
+	if (EIGHTH) //
+	{
+	}
+
+	else
+
+	if (NINTH) //
+	{
+	}
+
+	else
+
+	if (TENTH) //
+	{
+	}
+
+	else
+
+	if (ELEVENTH) //
+	{
+	}
+
+	else
+
+	if (TWELFTH) //
+	{
+	}
+
+	else
+
+	if (THIRTEENTH) //
+	{
+	}
+
+	else
+
+	if (FOURTEENTH) //
+	{
+	}
+
+	else
+
+	if (FIFTEENTH) //
+	{
+	}
+
+	else
+
+	if (SIXTEENTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTEENTH) //
+	{
+	}
+}
