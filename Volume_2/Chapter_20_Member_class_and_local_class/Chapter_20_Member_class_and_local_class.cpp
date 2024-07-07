bebd3abfad3a62f8f4dddfa07001a780e727667b
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-07 14:02:44 +0200

    Chapter_20_Member_class_and_local_class - currently skipped, not essential for further chapters.

---
+++ Volume_2/Chapter_20_Member_class_and_local_class/Chapter_20_Member_class_and_local_class.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Chapter_20_Member_class_and_local_class
+
+/* Description as in commit:
+Chapter_20_Member_class_and_local_class - currently skipped, not essential for further chapters.
+*/
+
+#define FIRST		0	//
+#define SECOND		0	//
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+#define SEVENTH		0	//
+#define EIGHTH		0	//
+#define NINTH		0	//
+#define TENTH		0	//
+#define ELEVENTH	0	//
+#define TWELFTH		0	//
+#define THIRTEENTH	0	//
+#define	FOURTEENTH	0	//
+#define FIFTEENTH	0	//
+#define SIXTEENTH	0	//
+#define SEVENTEENTH	0	//
+
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTH) //
+	{
+	}
+
+	else
+
+	if (EIGHTH) //
+	{
+	}
+
+	else
+
+	if (NINTH) //
+	{
+	}
+
+	else
+
+	if (TENTH) //
+	{
+	}
+
+	else
+
+	if (ELEVENTH) //
+	{
+	}
+
+	else
+
+	if (TWELFTH) //
+	{
+	}
+
+	else
+
+	if (THIRTEENTH) //
+	{
+	}
+
+	else
+
+	if (FOURTEENTH) //
+	{
+	}
+
+	else
+
+	if (FIFTEENTH) //
+	{
+	}
+
+	else
+
+	if (SIXTEENTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTEENTH) //
+	{
+	}
+}
