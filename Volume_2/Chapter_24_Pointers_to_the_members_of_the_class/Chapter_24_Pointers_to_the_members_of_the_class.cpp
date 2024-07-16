f61ab209fbee06e00342e2db39a33e867a53185c
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-16 08:38:13 +0200

    Chapter_24_Pointers_to_the_members_of_the_class - currently skiped

---
+++ Volume_2/Chapter_24_Pointers_to_the_members_of_the_class/Chapter_24_Pointers_to_the_members_of_the_class.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Chapter_24_Pointers_to_the_members_of_the_class
+
+/* Description as in commit:
+// Chapter_24_Pointers_to_the_members_of_the_class - currently skiped
+
+*/
+
+#define FIRST		0	//
+#define SECOND		0	// 
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+#define SEVENTH		0	//
+#define EIGHTH		0	//
+#define NINTH		0	//
+#define TENTH		0	//
+#define ELEVENTH	0	//
+#define TWELFTH		0	//
+#define THIRTEENTH	0	//
+#define	FOURTEENTH	0	//
+#define FIFTEENTH	0	//
+#define SIXTEENTH	0	//
+#define SEVENTEENTH	0	//
+
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTH) //
+	{
+	}
+
+	else
+
+	if (EIGHTH) //
+	{
+	}
+
+	else
+
+	if (NINTH) //
+	{
+	}
+
+	else
+
+	if (TENTH) //
+	{
+	}
+
+	else
+
+	if (ELEVENTH) //
+	{
+	}
+
+	else
+
+	if (TWELFTH) //
+	{
+	}
+
+	else
+
+	if (THIRTEENTH) //
+	{
+	}
+
+	else
+
+	if (FOURTEENTH) //
+	{
+	}
+
+	else
+
+	if (FIFTEENTH) //
+	{
+	}
+
+	else
+
+	if (SIXTEENTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTEENTH) //
+	{
+	}
+}
