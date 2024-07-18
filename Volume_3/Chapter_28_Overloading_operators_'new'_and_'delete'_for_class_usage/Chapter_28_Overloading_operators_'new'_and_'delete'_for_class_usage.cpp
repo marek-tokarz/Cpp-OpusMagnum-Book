cba7c8f0aa2ccd8107a7200e58f4191939f2f3f1
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-18 10:36:05 +0200

    Chapter_28_Overloading_operators_'new'_and_'delete'_for_class_usage - currently skipped

---
+++ Volume_3/Chapter_28_Overloading_operators_'new'_and_'delete'_for_class_usage/Chapter_28_Overloading_operators_'new'_and_'delete'_for_class_usage.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Chapter_28_Overloading_operators_'new'_and_'delete'_for_class_usage
+
+/* Description as in commit:
+Chapter_28_Overloading_operators_'new'_and_'delete'_for_class_usage - currently skipped
+*/
+
+#define FIRST		0	//
+#define SECOND		0	// 
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+#define SEVENTH		0	//
+#define EIGHTH		0	//
+#define NINTH		0	//
+#define TENTH		0	//
+#define ELEVENTH	0	//
+#define TWELFTH		0	//
+#define THIRTEENTH	0	//
+#define	FOURTEENTH	0	//
+#define FIFTEENTH	0	//
+#define SIXTEENTH	0	//
+#define SEVENTEENTH	0	//
+
+
+int main()
+{
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTH) //
+	{
+	}
+
+	else
+
+	if (EIGHTH) //
+	{
+	}
+
+	else
+
+	if (NINTH) //
+	{
+	}
+
+	else
+
+	if (TENTH) //
+	{
+	}
+
+	else
+
+	if (ELEVENTH) //
+	{
+	}
+
+	else
+
+	if (TWELFTH) //
+	{
+	}
+
+	else
+
+	if (THIRTEENTH) //
+	{
+	}
+
+	else
+
+	if (FOURTEENTH) //
+	{
+	}
+
+	else
+
+	if (FIFTEENTH) //
+	{
+	}
+
+	else
+
+	if (SIXTEENTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTEENTH) //
+	{
+	}
+}
