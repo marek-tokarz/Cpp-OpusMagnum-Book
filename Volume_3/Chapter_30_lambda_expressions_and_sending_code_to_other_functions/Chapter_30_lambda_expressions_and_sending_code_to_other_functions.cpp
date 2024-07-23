b7135e3c59f70951a7e48fe2a8a1e844621a3793
Marek Tokarz <marek.tokarz14@gmail.com>
2024-07-23 10:34:31 +0200

    Chapter_30_lambda_expressions_and_sending_code_to_other_functions - currently skipped

--- Volume_3/Chapter_30_lambda_expressions_and_sending_code_to_other_functions/Chapter_30_lambda_expressions_and_sending_code_to_other_functions.cpp
+++ Volume_3/Chapter_30_lambda_expressions_and_sending_code_to_other_functions/Chapter_30_lambda_expressions_and_sending_code_to_other_functions.cpp
@@ -1,20 +1,132 @@
-﻿// Chapter_30_lambda_expressions_and_sending_code_to_other_functions.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+// Chapter_30_lambda_expressions_and_sending_code_to_other_functions
+
+/* Description as in commit:
+Chapter_30_lambda_expressions_and_sending_code_to_other_functions - currently skipped
+*/
+
+#define FIRST		0	//
+#define SECOND		0	// 
+#define THIRD		0	//
+#define FOURTH		0	//
+#define FIFTH		0	//
+#define SIXTH		0	//
+#define SEVENTH		0	//
+#define EIGHTH		0	//
+#define NINTH		0	//
+#define TENTH		0	//
+#define ELEVENTH	0	//
+#define TWELFTH		0	//
+#define THIRTEENTH	0	//
+#define	FOURTEENTH	0	//
+#define FIFTEENTH	0	//
+#define SIXTEENTH	0	//
+#define SEVENTEENTH	0	//
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	if (FIRST) //
+	{
+	}
+
+	else
+
+	if (SECOND) //
+	{
+	}
+
+	else
+
+	if (THIRD) //
+	{
+	}
+
+	else
+
+	if (FOURTH) //
+	{
+	}
+
+	else
+
+	if (FIFTH) //
+	{
+	}
+
+	else
+
+	if (SIXTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTH) //
+	{
+	}
+
+	else
+
+	if (EIGHTH) //
+	{
+	}
+
+	else
+
+	if (NINTH) //
+	{
+	}
+
+	else
+
+	if (TENTH) //
+	{
+	}
+
+	else
+
+	if (ELEVENTH) //
+	{
+	}
+
+	else
+
+	if (TWELFTH) //
+	{
+	}
+
+	else
+
+	if (THIRTEENTH) //
+	{
+	}
+
+	else
+
+	if (FOURTEENTH) //
+	{
+	}
+
+	else
+
+	if (FIFTEENTH) //
+	{
+	}
+
+	else
+
+	if (SIXTEENTH) //
+	{
+	}
+
+	else
+
+	if (SEVENTEENTH) //
+	{
+	}
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
